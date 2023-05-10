d444349bd321c25e85f42c93a630fe84775e93dd
arielpriarone <arielpriarone@gmail.com>
2023-05-10 15:38:39 +0200

    test

--- src/data/data.py
+++ src/data/data.py
@@ -3,9 +3,12 @@ import numpy as np
 import seaborn as sb
 import os
 
-# folder path
-dirPath = "./data/raw/1st_test_IMSBearing/"
-fileName = "2003.10.22.12.06.24"
 
-rawData=pd.read_csv(dirPath+fileName,delimiter='\t',names=["Bearing 1 x", "Bearing 1 y", "Bearing 2 x", "Bearing 2 y","Bearing 3 x", "Bearing 3 y", "Bearing 4 x", "Bearing 4 y"])
-print('\n'+rawData.head)
+class snapshot:
+    def __init__(self,rawData=None):
+        self.rawData=rawData
+    def readImsFile(self,path):
+        # read a ims formatted file data - ref: The  data  was  generated  by  the  NSF  I/UCR  Center  for  Intelligent  Maintenance  Systems  
+        # (IMS  – www.imscenter.net) with support from Rexnord Corp. in Milwaukee, WI. 
+        self.rawData=pd.read_csv(path,delimiter='\t',names=["Bearing 1 x", "Bearing 1 y", "Bearing 2 x", "Bearing 2 y","Bearing 3 x", "Bearing 3 y", "Bearing 4 x", "Bearing 4 y"])
+
