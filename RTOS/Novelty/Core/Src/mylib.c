f7d4bfd49c5ee302639a9b0055bda07836b79d1e
arielpriarone <arielpriarone@gmail.com>
2023-11-29 18:58:25 +0100

    poacket power computed but problem with reference address

--- RTOS/Novelty/Core/Src/mylib.c
+++ RTOS/Novelty/Core/Src/mylib.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 #include "wavelib.h"
+
+double norm2(double *array, int length);
+double* packetCoeff(double *inp, int length, int J);
 
 int add_int(int *start, int *len){
 	int result = 0;
@@ -18,6 +22,7 @@ void uint16ArrayToString(const uint16_t *uintArray, size_t length, char *result)
 		result += strlen(result);
 	}
 	sprintf(result, "%u", uintArray[i]);  // Last element without a tab
+	return;
 }
 
 void printarray(const uint16_t *uintarray, size_t length){
@@ -25,56 +30,81 @@ void printarray(const uint16_t *uintarray, size_t length){
 		printf("%u\r\n", uintarray[var]);
 	}
 	printf("\r\n");
+	return;
 }
 
-SnapReadyCallback(const uint16_t *uintarray, size_t length){
-	printf("Snapshot acquired. \r\nTimestamp: ");
+void printFloatArray(double *inp, size_t length){
+	for (int var = 0; var < length; ++var) {
+		printf("%e \r\n", inp[var]);
+	}
+	printf(" \r\n");
+	return;
+}
+
+void SnapReadyCallback(const uint16_t *uintarray, size_t length){
+	// first convert the array to packet coef
+	int three_depth = 6; 		// depth of the decomposition tree
+	int out_len = round(pow(2,three_depth)); //the number of bottom level nodes
+	double snap_time_domain[length]; // alloc the double array
+	for (int i = 0; i < length; ++i) { // Cast each element from int to double
+		snap_time_domain[i] = (double)uintarray[i];
+	}
+
+	double *coefs = packetCoeff(snap_time_domain, (int)length, three_depth);
+	printf("Snapshot features acquired. \r\nTimestamp: ");
 	printTimestamp();
-	printf(" \r\nStart data. \r\n");
-	printarray(uintarray, length);
-	printf("End data.");
+	printf(" \r\nStart features. \r\n");
+	printFloatArray(coefs, out_len); kjfsalkl
+	printf("End features.");
+	return;
 }
 
 
-void PacketTrasform(){
-	int i, J, N, len;
-	int X, Y;
+double* packetCoeff(double *inp, int length, int J) { // compute the power of each packet coefficient in the lowest level
+	int N, len;
+	int coef_len;
+	coef_len = round(pow(2,J)); // the nodes in the lowest level are 2^depth
+	double coefs[coef_len];
+
 	wave_object obj;
 	wtree_object wt;
-	double *inp, *oup;
 
-	char *name = "db3";
-	obj = wave_init(name);// Initialize the wavelet
-	N = 147;
-	inp = (double*)malloc(sizeof(double)* N);
-	for (i = 1; i < N + 1; ++i) {
-		inp[i - 1] = -0.25*i*i*i + 25 * i *i + 10 * i;
-	}
-	J = 3;
+	char *name = "db10";
+	obj = wave_init(name); // Initialize the wavelet
+	N = length;
 
-	wt = wtree_init(obj, N, J);// Initialize the wavelet transform object
-	setWTREEExtension(wt, "sym");// Options are "per" and "sym". Symmetric is the default option
+	wt = wtree_init(obj, N, J); // Initialize the wavelet transform object
+	setWTREEExtension(wt, "sym"); // Options are "per" and "sym". Symmetric is the default option
 
 	wtree(wt, inp);
 	wtree_summary(wt);
-	X = 3;
-	Y = 5;
-	len = getWTREENodelength(wt, X);
+
+	len = getWTREENodelength(wt, J); //because the lowest level is J
 	printf(" \r\n %d", len);
 	printf(" \r\n");
-	oup = (double*)malloc(sizeof(double)* len);
 
-	printf("Node [%d %d] Coefficients :  \r\n",X,Y);
-	getWTREECoeffs(wt, X, Y, oup, len);
-	for (i = 0; i < len; ++i) {
-		printf("%g ", oup[i]);
+	double* oup = (double *)malloc(sizeof(double) * len);
+
+	for(int node_index = 0; node_index < coef_len; node_index++){
+		printf("Node [%d %d] Coefficients :  \r\n", J, node_index);
+		getWTREECoeffs(wt, J, node_index, oup, len);
+		coefs[node_index] = norm2(oup,len);
 	}
-	printf(" \r\n");
 
 	free(inp);
-	free(oup);
 	wave_free(obj);
 	wtree_free(wt);
-	return;
+
+	return coefs;
+}
+
+double norm2(double *array, int length) {
+	double sum = 0.0;
+
+	for (int i = 0; i < length; ++i) {
+		sum += array[i] * array[i];
+	}
+
+	return sqrt(sum);
 }
 
