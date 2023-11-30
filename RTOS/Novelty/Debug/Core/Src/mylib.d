5b6611a61224a31c9ba9837cc4b42747d2b64b5f
arielpriarone <arielpriarone@gmail.com>
2023-11-30 16:17:17 +0100

    removed .gitignore files

--- RTOS/Novelty/Debug/Core/Src/mylib.d
+++
@@ -1,3 +0,0 @@
-Core/Src/mylib.o: ../Core/Src/mylib.c \
- C:/Users/ariel/Documents/Courses/Tesi/Code/RTOS/Novelty/wavelib/header/wavelib.h
-C:/Users/ariel/Documents/Courses/Tesi/Code/RTOS/Novelty/wavelib/header/wavelib.h:
