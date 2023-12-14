8b54d752ab7fad892fbd8752b93e1d40ba9f0fc3
arielpriarone <arielpriarone@gmail.com>
2023-12-14 19:17:06 +0100

    created vornoi diagrams

--- notebooks/dummy.py
+++ notebooks/dummy.py
@@ -1,84 +1,107 @@
-from re import S
-import matplotlib
 import numpy as np
-import scipy.optimize as opt
 import matplotlib.pyplot as plt
-from rich import print
-matplotlib.use('Qt5Agg')
+from scipy.spatial import Voronoi
 
-def f(x,a,b,c)->np.ndarray: #function to fit
-    return a*np.exp(b*x)+c
+def voronoi_finite_polygons_2d(vor, radius=None):
+    """
+    Reconstruct infinite voronoi regions in a 2D diagram to finite
+    regions.
 
-xs=np.random.uniform(0,10,100)
-xs.sort()
+    Parameters
+    ----------
+    vor : Voronoi
+        Input diagram
+    radius : float, optional
+        Distance to 'points at infinity'.
 
-err=np.random.uniform(-10,10,100) #noise
-y=f(xs,2,0.5,0.1)
-y_err = y + err
+    Returns
+    -------
+    regions : list of tuples
+        Indices of vertices in each revised Voronoi regions.
+    vertices : list of tuples
+        Coordinates for revised Voronoi vertices. Same as coordinates
+        of input vertices, with 'points at infinity' appended to the
+        end.
 
-params, cv = opt.curve_fit(f, xs, y_err, p0=[1,1,1]) #fitting
-x=np.random.uniform(0,15,100)
-x.sort()
-y_fit = f(x,*params)
+    """
 
-# new procedure
-# fit the function a+b*e^(c*x)
-# x_f=np.array([-0.99,-0.945,-0.874,-0.859,-0.64,-0.573,-0.433,-0.042,-0.007,0.054,0.088,0.222,0.401,0.465,0.633,0.637,0.735,0.762,0.791,0.981])
-# y_f=np.array([0.418,0.412,0.452,0.48,0.453,0.501,0.619,0.9,0.911,0.966,0.966,1.123,1.414,1.683,2.101,1.94,2.473,2.276,2.352,3.544])
-x_f=xs.copy()
-y_f=y_err.copy()
-S=np.array([0])
-for k in range(1,len(x_f)):
-    S=np.append(S,S[-1]+0.5*(y_f[k]+y_f[k-1])*(x_f[k]-x_f[k-1]))
+    if vor.points.shape[1] != 2:
+        raise ValueError("Requires 2D input")
 
-A=np.linalg.norm(x_f-x_f[0])**2
-B=np.sum((x_f-x_f[0])*S)
-C=B
-D=np.linalg.norm(S)**2
-MAT1=np.matrix([[A,B],[C,D]])
-print(MAT1)
+    new_regions = []
+    new_vertices = vor.vertices.tolist()
 
-A=np.sum((y_f-y_f[0])*(x_f-x_f[0]))
-B=np.sum((y_f-y_f[0])*S)
-MAT2=np.matrix([[A],[B]])
-print(MAT2)
+    center = vor.points.mean(axis=0)
+    if radius is None:
+        radius = vor.points.ptp().max()
 
-RES=MAT1**(-1)*MAT2
-print(RES)
-A1=RES[0]
-B1=RES[1]
+    # Construct a map containing all ridges for a given point
+    all_ridges = {}
+    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+        all_ridges.setdefault(p1, []).append((p2, v1, v2))
+        all_ridges.setdefault(p2, []).append((p1, v1, v2))
 
-a1=-A1/B1
-c1=B1.copy()
-c2=B1.copy()
+    # Reconstruct infinite regions
+    for p1, region in enumerate(vor.point_region):
+        vertices = vor.regions[region]
 
-theta=np.exp(c2*x_f)
+        if all(v >= 0 for v in vertices):
+            # finite region
+            new_regions.append(vertices)
+            continue
 
-A=x_f.size
-B=np.sum(theta)
-C=B.copy()
-D=np.linalg.norm(theta)**2
-MAT1=np.matrix([[A,B],[C,D]])
+        # reconstruct a non-finite region
+        ridges = all_ridges[p1]
+        new_region = [v for v in vertices if v >= 0]
 
-A=np.sum(y_f)
-B=np.sum(y_f*theta.reshape(-1,1))
-MAT2=np.matrix([[A],[B]])
+        for p2, v1, v2 in ridges:
+            if v2 < 0:
+                v1, v2 = v2, v1
+            if v1 >= 0:
+                # finite ridge: already in the region
+                continue
 
-RES=MAT1**(-1)*MAT2
-A2=RES[0]
-B2=RES[1]
-params = np.array([B2,c2,A2]).reshape(-1).tolist()
-print(params)
-y_french = f(x,*params) #french method
+            # Compute the missing endpoint of an infinite ridge
 
-fig, ax = plt.subplots()
-ax.plot(xs,y)
-ax.scatter(xs,y_err)
-ax.plot(x,y_fit)
-ax.plot(x,y_french)
+            t = vor.points[p2] - vor.points[p1] # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
 
+            midpoint = vor.points[[p1, p2]].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[v2] + direction * radius
 
+            new_region.append(len(new_vertices))
+            new_vertices.append(far_point.tolist())
+
+        # sort region counterclockwise
+        vs = np.asarray([new_vertices[v] for v in new_region])
+        c = vs.mean(axis=0)
+        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        new_region = np.array(new_region)[np.argsort(angles)]
+
+        # finish
+        new_regions.append(new_region.tolist())
+
+    return new_regions, np.asarray(new_vertices)
+
+# make up data points
+np.random.seed(1234)
+points = np.random.rand(15, 2)
+
+# compute Voronoi tesselation
+vor = Voronoi(points)
+
+# plot
+regions, vertices = voronoi_finite_polygons_2d(vor)
+
+# colorize
+for region in regions:
+    polygon = vertices[region]
+    plt.fill(*zip(*polygon), alpha=0.4)
+
+plt.plot(points[:,0], points[:,1], 'ko')
+plt.xlim(vor.min_bound[0] - 0.1, vor.max_bound[0] + 0.1)
+plt.ylim(vor.min_bound[1] - 0.1, vor.max_bound[1] + 0.1)
 
 plt.show()
-
-
