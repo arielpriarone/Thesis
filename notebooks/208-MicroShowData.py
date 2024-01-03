1f48b0f309e0783d16e407863a766a5d70a0a5f2
arielpriarone <arielpriarone@gmail.com>
2024-01-03 21:58:55 +0100

    code prepared for tests

--- notebooks/208-MicroShowData.py
+++ notebooks/208-MicroShowData.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
+from scipy.fft import rfft, rfftfreq
 
 # Read the CSV files into dataframes
 df_features = pd.read_csv(r"C:\Users\ariel\Documents\Courses\Tesi\Code\train_data.csv", sep='\t').dropna(axis=1)
@@ -13,7 +14,7 @@ df_timeseries = pd.read_csv(r"C:\Users\ariel\Documents\Courses\Tesi\Code\timeser
 # standardise the features
 df_features.iloc[:, 1:] = (df_features.iloc[:, 1:] - df_features.iloc[:, 1:].mean()) / df_features.iloc[:, 1:].std()
 
-# Create a 3D plot for dataframe of features
+# Create a plot for dataframe of features
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
 colormap = cm.get_cmap('cool')
@@ -31,19 +32,35 @@ ax1.set_ylabel('Features')
 ax1.tick_params(labelrotation=45)
 ax1.legend()
 
-import src
-fig, ax = plt.subplots()
+#Create a plot for dataframe of timeseries
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
 for timestamp in df_timeseries['Timestamp'].unique():
     df_timeseries_timestamp = df_timeseries[df_timeseries['Timestamp'] == timestamp]
-    result, freqs, _ = src.features.FFT(df_timeseries_timestamp.to_xarray(), 5000, preproc=None)
-    ax.plot(freqs, result)
+    ax2.plot(df_timeseries_timestamp.columns[1:], df_timeseries_timestamp.values[0, 1:], label=timestamp)
 
-# Create a 3D plot for dataframe of timeseries
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
-# for timestamp in df_timeseries['Timestamp'].unique():
-#     df_timeseries_timestamp = df_timeseries[df_timeseries['Timestamp'] == timestamp]
-#     ax2.plot(df_timeseries_timestamp.columns[1:], df_timeseries_timestamp.values[0, 1:], label=timestamp)
+# plot the spectrum of the timeseries
+fig, axs = plt.subplots()
+sampling_rate = 5000 # Hz
+N=6000 # number of samples
+i=0
+for timestamp in df_timeseries['Timestamp'].unique():
+    if i < 10:
+        color = 'blue'
+    else:
+        color = 'red'
+    df_timeseries_timestamp = df_timeseries[df_timeseries['Timestamp'] == timestamp]
+    # normalize the timeseries
+    fourier_timeserie = (df_timeseries_timestamp.values[0, 1:]-np.mean(df_timeseries_timestamp.values[0, 1:]))
+    # preprocess hann window
+    hann_window = np.hanning(N)
+    fourier_timeserie = np.multiply(fourier_timeserie, hann_window)
+    # plot the spectrum
+    axs.plot(rfftfreq(N, d=1/sampling_rate), 2*np.abs(rfft(fourier_timeserie))/N,color=color)
+    i+=1
+axs.set_xlabel('Frequency[Hz]')
+axs.set_ylabel('Amplitude')
+axs.set_yscale('log')
 
 # Show the plots
 plt.show()
