4577010d5092c62710b0645ef7d6990c87449563
arielpriarone <arielpriarone@gmail.com>
2024-02-15 13:02:00 +0100

    finished analisys on shaker

--- notebooks/966-Shaker02.py
+++ notebooks/966-Shaker02.py
@@ -1,15 +1,24 @@
+from calendar import c
+from itertools import count
+from operator import le
 from os import times
 from pathlib import Path
+from matplotlib import lines
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 from rich import print
 import src
+from sklearn.preprocessing import StandardScaler
 src.visualization.set_matplotlib_params()
 
-filepath = r"data\processed\Shaker_test02\test_data_shaker.csv"
+from pandas.plotting import scatter_matrix
+
+filepath_test = r"data\processed\Shaker_test02\test_data_shaker.csv"
+filepath_train = r"data\processed\Shaker_test02\train_data_shaker.csv"
 tests_separators = [10,10,10,10,10,10,10,10,10,10,10]; # number of samples per test
+separator_index = [0] + list(np.cumsum(tests_separators)) # index of the start of each test
 tests_names = [ "$V_{pp} =  580$mV",
                 "$V_{pp} = 1000$mV",
                 "$V_{pp} = 1980$mV",
@@ -21,11 +30,29 @@ tests_names = [ "$V_{pp} =  580$mV",
                 "$V_{pp} = 2120$mV",
                 "$V_{pp} = 2000$mV",
                 "$V_{pp} = 1540$mV (second wave)"]
+# model of the micro
+model = {
+    'n_clusters': 4,
+    'centers': [
+        [0.7107763495560776, 0.13504565182945946, -0.8775567952867753, 0.4311111009431372, -0.9116528401783603, -0.7639974849126245, -0.8826249186556334],
+        [-1.703008731888498, -1.4734313674427566, 1.5333927526890774, 0.18434453893877897, 1.4180484963527293, 0.8985265324389914, 1.4922919056340718],
+        [0.2887162150172579, 1.2046332459821292, 0.21272680742067332, 0.2408753393311418, 0.3974166413880705, 0.21138671298908396, 0.25709648810500824],
+        [-1.7032627631307642, -1.4733359102427546, 1.5337568568739655, -1.9542070763058088, 1.41592185337629, 1.582596115019071, 1.504017229610121]
+    ],
+    'stds': [72.95143809510259, 575891341.8682002, 200578.32468086746, 7940.905464949911, 15427.628903013298, 1915.0306669815125, 4594.875000274002],
+    'means': [1847.6834319131162, 22001878406.922306, 247723.75760067653, 157839.1469004324, 32169.004794538432, 2722.5224566287525, 8479.908734171591],
+    'radiuses': [1.2828838566475935, 1.143139176317999, 1.9723564304733, 1.7256753322678466],
+    'weights': [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+}
+
+
 
 # %% Load the data
-data = pd.read_csv(filepath, sep='\t')
+data = pd.read_csv(filepath_test, sep='\t')
 print(data.keys())  
 print(data.head())
+
+train_data = pd.read_csv(filepath_train, sep='\t')
 
 NoveltyMetric = data["Novelty"].to_numpy()
 Timestamp_all = data["Timestamp"].to_numpy()
@@ -59,5 +86,43 @@ ax.set_xticklabels([])
 ax.set_xlabel("snapshots")
 ax.set_ylabel("Novelty Metric [%]")
 fig.legend(ncol=3, loc='outside upper right')
+
+# plot the features in s confusion matrix
+features = train_data.keys()[1:-1]
+
+features_matrix = train_data[features].to_numpy()
+
+stdsclr=StandardScaler()
+features_matrix = stdsclr.fit_transform(features_matrix)
+features_matrix_test = stdsclr.transform(data[features].to_numpy())
+
+print(features)
+
+fig, ax = plt.subplots(len(features),len(features), figsize=(5.78851,7.18))
+for i, feature1 in enumerate(features):
+    for j, feature2 in enumerate(features):
+        if i==j:
+            ax[i,j].hist(features_matrix[:,j], bins='auto', color='k')
+        else:
+            ax[i,j].scatter(features_matrix[:,j],features_matrix[:,i], c='k', s=2, marker='.')
+            ax[i,j].scatter(features_matrix_test[separator_index[6]:separator_index[7],j],features_matrix_test[separator_index[6]:separator_index[7],i], c='r', s=2, marker='.')
+            ax[i,j].scatter(features_matrix_test[separator_index[8]:separator_index[9],j],features_matrix_test[separator_index[8]:separator_index[9],i], c='m', s=2, marker='.')
+            for cluster in range(model['n_clusters']):
+                ax[i,j].add_patch(plt.Circle((model['centers'][cluster][j],model['centers'][cluster][i]),model['radiuses'][cluster],color='gray',fill=True, alpha=0.2, linestyle=''))
+        ax[i,j].xaxis.set_tick_params(labelbottom=False)
+        ax[i,j].yaxis.set_tick_params(labelleft=False)
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
+
+        if i==len(features)-1:
+            ax[i,j].set_xlabel(feature2)
+
+        if j==0:
+            ax[i,j].set_ylabel(feature1)
+
+fig.tight_layout()
+plt.subplots_adjust(wspace=0, hspace=0)
+            
+
 
 plt.show()
