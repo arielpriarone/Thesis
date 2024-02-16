fc6991c663ba7d83441c3b8fbe9b129460009318
arielpriarone <arielpriarone@gmail.com>
2024-02-16 15:23:13 +0100

    started predictions

--- notebooks/953-Novelty500samples.py
+++ notebooks/953-Novelty500samples.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 import time
+import matplotlib as mpl
+from matplotlib.lines import lineStyles
 from pymongo import MongoClient
 from rich import print
 import matplotlib.pyplot as plt
@@ -45,5 +47,48 @@ ax.set_xlim(dt.datetime.fromisoformat("2003-11-13T07:00"), dt.datetime.fromisofo
 ax.set_ylim(0,10000)
 ax.legend()
 
+# rul calculation
+from bisect import bisect_right
+
+timestamps_float = [ts.timestamp() for ts in timestamps]
+offset = timestamps_float[0]  # offset to set the first timestamp to 0
+timestamps_float = [ts - offset for ts in timestamps_float]  # set the first timestamp to 0
+print(f"type(timestamps_float): {type(timestamps_float)}")
+print(f"type(timestamps_float[0]): {type(timestamps_float[0])}")
+PRED_INSTANTS = ["2003-11-16 16:49",
+"2003-11-19 15:05",
+"2003-11-20 16:00",
+"2003-11-22 06:32",
+"2003-11-23 12:00",
+"2003-11-24 17:27"]
+fig, ax = plt.subplots()
+ax.scatter(timestamps,novelty_metric,c='k',marker='.', s=2, label='Novelty metric')
+colormap = [mpl.colors.to_hex(plt.cm.tab10(i)) for i in range(len(PRED_INSTANTS))]
+timestamp_plot = [timestamp_float for timestamp_float in np.linspace(timestamps_float[0],timestamps_float[-1]+86400*2,500)]
+for ind, instant in enumerate([dt.datetime.fromisoformat(aux).timestamp()-offset for aux in PRED_INSTANTS]):
+    #time_of_prediction = dt.datetime.fromisoformat("2003-11-21T00:00").timestamp() - offset  # time of prediction "2003-11-16T07:46"
+    time_of_prediction = instant
+    windowing = 230 # windowing for the prediction
+
+    print(f"type(time_of_prediction): {type(time_of_prediction)}")
+    print(f"time_of_prediction: {time_of_prediction}")
+    print(f"timestamps_float[0]: {timestamps_float[0]}")
+    index = bisect_right(timestamps_float, time_of_prediction)
+    print(f"Index of prediction: {index}")
+
+    a,b,c = src.ExpRegressor(timestamps_float[index-windowing:index], novelty_metric[index-windowing:index]) #Fits the function a*exp(b*x)+c to the given data points.
+    print (f"a: {a}, b: {b}, c: {c}")
+    
+    predictions = a * np.exp(b * np.array(timestamp_plot)) + c
+
+    ax.axvline(timestamps[index], color=colormap[ind], linestyle='dashed',linewidth=1)
+    ax.plot([dt.datetime.fromtimestamp(timestamp_float+offset) for timestamp_float in timestamp_plot],
+            predictions, label=PRED_INSTANTS[ind], color=colormap[ind], linewidth=1)
+    
+ax.axhline(7000, color='k', linestyle='dashed',label= 'RUL threshold',linewidth=1)
+ax.set_ylim(-300,7500)  
+ax.set_xlabel("Timestamp")
+ax.set_ylabel("Novelty metric [%]")  
+ax.legend()
 
 plt.show()
