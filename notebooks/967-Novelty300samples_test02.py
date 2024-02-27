b63b165c44b511e7de7d257ec960c289e466fb50
arielpriarone <arielpriarone@gmail.com>
2024-02-27 17:27:05 +0100

    FD on test 02 IMS

--- notebooks/967-Novelty300samples_test02.py
+++ notebooks/967-Novelty300samples_test02.py
@@ -55,8 +55,7 @@ ax.set_xlabel("Timestamp")
 ax.set_ylabel("Novelty metric [%]")
 ax.set_yscale('symlog')
 ax.annotate('Novel behaviour\n2004-02-16 03:32', xy=(dt.datetime(2004,2,16,3,32),10), xytext=(dt.datetime(2004,2,16,12,0),-10), arrowprops=dict(facecolor='black', arrowstyle='->'))
-ax.xaxis.set_major_formatter(
-    mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
 ax.legend()
 
 
@@ -92,12 +91,11 @@ ax.set_ylim(-300,7500)
 ax.set_xlabel("Timestamp")
 ax.set_ylabel("Novelty metric [%]")
 ax.set_xlim(timestamps[0],timestamps[-1])
-ax.xaxis.set_major_formatter(
-    mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
 ax.legend()
 
 # figure 3 - Fault detection
-threshold = 70 # threshold for fault detection [%]
+threshold = 0 # threshold for fault detection [%]
 document = data.find_one({"_id": "Kmeans cluster fault indicator"})
 timestamps = document["timestamp"]
 if not all(timestamps[i] < timestamps[i+1] for i in range(len(timestamps)-1)): raise ValueError("Timestamps are not in order")
@@ -122,12 +120,53 @@ timestamp_plot = [timestamp_float for timestamp_float in np.linspace(timestamps_
 ax.axhline(threshold, color='k', linestyle='dotted',label= 'Threshold',linewidth=1)
 #ax.axhline(7000, color='k', linestyle='dashed',label= 'RUL threshold',linewidth=1)
 # ax.set_ylim(-300,7500)  
-ax.set_xlabel("Timestamp")
-ax.set_ylabel("Fault metric [%]")
+#ax.set_xlabel("Timestamp")
+#ax.set_ylabel("Fault metric [%]")
 # ax.set_yscale('symlog')
-# ax.annotate('Novel behaviour\n2004-02-16 03:32', xy=(dt.datetime(2004,2,16,3,32),10), xytext=(dt.datetime(2004,2,16,12,0),-10), arrowprops=dict(facecolor='black', arrowstyle='->'))
+ax.annotate('Faulty behaviour\n2004-02-18 06:13', xy=(dt.datetime(2004,2,18,6,13),threshold), xytext=(dt.datetime(2004,2,16,12,0),25), arrowprops=dict(facecolor='black', arrowstyle='->'))
+#ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+#ax.legend()
+
+
+# rul calculation
+from bisect import bisect_right
+
+timestamps_float = [ts.timestamp() for ts in timestamps]
+offset = timestamps_float[0]  # offset to set the first timestamp to 0
+timestamps_float = [ts - offset for ts in timestamps_float]  # set the first timestamp to 0
+print(f"type(timestamps_float): {type(timestamps_float)}")
+print(f"type(timestamps_float[0]): {type(timestamps_float[0])}")
+PRED_INSTANTS = ["2004-02-18 06:13"]#, "2004-02-18 18:13"]
+#fig, ax = plt.subplots()
+#ax.scatter(timestamps,fault_metric,c='k',marker='.', s=2, label='Fault metric')
+colormap = [mpl.colors.to_hex(plt.cm.tab10(i)) for i in range(len(PRED_INSTANTS))]
+timestamp_plot = [timestamp_float for timestamp_float in np.linspace(timestamps_float[0],timestamps_float[-1]+86400*2,500)]
+for ind, instant in enumerate([dt.datetime.fromisoformat(aux).timestamp()-offset for aux in PRED_INSTANTS]):
+    #time_of_prediction = dt.datetime.fromisoformat("2003-11-21T00:00").timestamp() - offset  # time of prediction "2003-11-16T07:46"
+    time_of_prediction = instant
+    windowing = 50 # windowing for the prediction
+
+    print(f"type(time_of_prediction): {type(time_of_prediction)}")
+    print(f"time_of_prediction: {time_of_prediction}")
+    print(f"timestamps_float[0]: {timestamps_float[0]}")
+    index = bisect_right(timestamps_float, time_of_prediction)
+    print(f"Index of prediction: {index}")
+
+    a,b,c = src.ExpRegressor(timestamps_float[index-windowing:index], fault_metric[index-windowing:index]) #Fits the function a*exp(b*x)+c to the given data points.
+    print (f"a: {a}, b: {b}, c: {c}")
+    
+    predictions = a * np.exp(b * np.array(timestamp_plot)) + c
+
+    ax.axvline(timestamps[index], color=colormap[ind], linestyle='dashed',linewidth=1)
+    ax.plot([dt.datetime.fromtimestamp(timestamp_float+offset) for timestamp_float in timestamp_plot],
+            predictions, label=PRED_INSTANTS[ind], color=colormap[ind], linewidth=1)
+    
+ax.axhline(7000, color='k', linestyle='dashed',label= 'RUL threshold',linewidth=1)
 ax.xaxis.set_major_formatter(
     mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+ax.set_ylim(-100,600)  
+ax.set_xlabel("Timestamp")
+ax.set_ylabel("Fault metric [%]")  
 ax.legend()
 
 plt.show()
