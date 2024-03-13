e7b3b81fa68828097255e2dd718b804006c50796
arielpriarone <arielpriarone@gmail.com>
2024-03-13 15:15:06 +0100

    latex diff working!

---
+++ LaTeX/Summary_diff/diff.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+set "old_path=..\Summary_v1\"
+set "new_path=..\Summary_v2\"
+set "doc_name_filename=Summary"
+
+echo Generate %doc_name_filename%_flat.tex for %new_path%
+cd %new_path%
+latexpand %doc_name_filename%.tex > %doc_name_filename%_flat.tex
+
+echo Generate %doc_name_filename%_flat.tex for %old_path%
+cd %old_path%
+latexpand %doc_name_filename%.tex > %doc_name_filename%_flat.tex
+
+echo Generate diff
+cd %new_path%
+latexdiff %old_path%%doc_name_filename%_flat.tex %doc_name_filename%_flat.tex > diff.tex
+latexmk  -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir=%OUTDIR% %DOC% -shell-escape diff.tex
+echo PDF generated in case of problems see diff.log
+
+echo Cleaning up
+del %doc_name_filename%_flat.tex
+del %old_path%%doc_name_filename%_flat.tex
+
+pause
