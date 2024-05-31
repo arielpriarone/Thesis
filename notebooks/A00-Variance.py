variance error corrected

notebooks/A00-Variance.py
@@ -1,21 +1,56 @@
+from calendar import c
+from operator import le
+from statistics import variance
+from matplotlib import axis
 import numpy as np
 import matplotlib.pyplot as plt
 from rich import print
+import matplotlib as mpl
 
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+import src
+
+src.vis.set_matplotlib_params()
 
 # generate three blobs
-X, y = make_blobs(n_samples=400, centers=3, n_features=2, random_state=1)
-
+centers=[(1,1), (12,16), (27,2)]
+X, y = make_blobs(n_samples=600, n_features=2, random_state=2, cluster_std=[1.5,3,1.5], centers=centers)
+X=np.append(X,np.array([19,1]).reshape(-1,2), axis=0) # add an outlier
+y=np.append(y,np.max(y)) # add an outlier label
+print(f"X.shape = {X.shape}")
+print(y.shape)
+print(y)
 # plot the blobls
+
+colorlist = ["#ebac23", "#b80058", "#008cf9", "#006e00", "#00bbad", "#d163e6", "#b24502", "#ff9287", "#5954d6", "#00c6f8", "#878500", "#00a76c", "#bdbdbd"]
+cmap=mpl.colors.ListedColormap(colorlist[0:len(np.unique(y))])
 fig, ax = plt.subplots()
-# generate a colormap with two colors defined as hex
-cmap = plt.cm.colors.ListedColormap(['#002B49', '#EF7B00', '#FFC72C'])
 ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, marker='.', s=10)
 
+# plot the centers
+for center in centers:
+    ax.scatter(center[0], center[1], c='black', marker='x', s=100)
+# compute the radiuses
+radiuses = dict()
+stds = dict()
+for center,y_int in zip(centers, np.arange(len(centers))): # for each cluster
+    current_pts = X[y==y_int, :]
+    N = current_pts.shape[0]
+    print(f"Y_int={y_int}, \t current data = {current_pts}")
+    dists = np.linalg.norm(current_pts-center, axis=1,ord=2)
+    radiuses[y_int] = np.max(dists)
+    stds[y_int] = np.sqrt(np.sum((np.linalg.norm(current_pts-center,ord=2))**2)/N)
+print(radiuses)
+print(stds)
+
+# plot the radiuses and stds
+for center,y_int in zip(centers, np.unique(y)):
+    ax.add_patch(plt.Circle(center, stds[y_int], fill=False, color='k', linestyle='--', linewidth=1))
+    ax.add_patch(plt.Circle(center, radiuses[y_int], fill=False, color='k', linestyle='-', linewidth=1))
+
 ax.set_aspect('equal')
-ax.set_xlabel('X[0]')
-ax.set_ylabel('X[1]')
+ax.set_xlabel('F_1')
+ax.set_ylabel('F_2')
 
 plt.show()

notebooks/dummy.py
@@ -1,119 +1,40 @@
-from math import remainder
-from gtts import gTTS
-import os
-from rich import print
-import datetime as dt
-from mutagen.mp3 import MP3
-from pydub import AudioSegment
-import io
+
+from colorspacious import cspace_converter
+
+import matplotlib.pyplot as plt
 import numpy as np
-from pydub import AudioSegment
 
-def delete_files(file_list):
-    for file in file_list:
-        try:
-            os.remove(file)
-            print(f"File '{file}' deleted successfully.")
-        except OSError as e:
-            print(f"Error deleting file '{file}': {e}")
+import matplotlib as mpl
+cmaps = {}
+
+gradient = np.arange(0,20)
+gradient = np.vstack((gradient, gradient))
 
 
-def join_mp3_files(input_files, output_file):
-    # Initialize an empty AudioSegment object to store the combined audio
-    combined_audio = AudioSegment.empty()
+def plot_color_gradients(category, cmap_list):
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list)
+    figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22
+    fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh))
+    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
+                        left=0.2, right=0.99)
+    axs[0].set_title(f'{category} colormaps', fontsize=14)
 
-    # Iterate over each input file
-    for file in input_files:
-        # Load the MP3 file
-        audio_segment = AudioSegment.from_mp3(file)
-        # Append the loaded audio to the combined audio
-        combined_audio += audio_segment
+    for ax, name in zip(axs, cmap_list):
+        ax.scatter(np.arange(10), np.zeros(10), c=np.arange(10), cmap=name)
+        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
 
-    # Export the combined audio to an MP3 file, overwriting if it already exists
-    combined_audio.export(output_file, format="mp3", codec="libmp3lame")
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axs:
+        ax.set_axis_off()
 
-class time:
-    def __init__(self, hh=0, mm=0, ss=0):
-        self.ss = ss % 60 # seconds
-        self.mm = (mm + ss//60) % 60 # minutes
-        self.hh = (hh + mm//60) % 24 # hours
-    def __str__(self):
-        out = ""
-        if self.hh > 0:
-            out += str(self.hh) + " hours "
-        if self.mm > 0:
-            out += str(self.mm) + " minutes "
-        if self.ss > 0:
-            out += str(self.ss) + " seconds"
-        return out
-    def __repr__(self):
-        return str(self)
+    # Save colormap list for later.
+    cmaps[category] = cmap_list
 
-def generate_silent_mp3(duration_seconds, output_file_path="silent.mp3"):
-    # Create a silent audio segment
-    silence = AudioSegment.silent(duration=duration_seconds * 1000)  # duration in milliseconds
+plot_color_gradients('Qualitative',
+                    ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2',
+                    'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b',
+                    'tab20c'])
 
-    # Save the silent audio segment to a file
-    silence.export(output_file_path, format="mp3")
-
-    
-def get_mp3_duration(file_path):
-    try:
-        audio = MP3(file_path)
-        duration_in_seconds = audio.info.length
-        return duration_in_seconds
-    except Exception as e:
-        print("Error:", e)
-        return None
-
-
-def text_to_speech(text, filename="speech.mp3"):
-    tts = gTTS(text=text, 
-               lang='en',
-               tld='com',
-               slow=False)
-    tts.save(filename)
-    #os.system(f"start {filename}")
-
-if __name__ == "__main__":
-    i=0
-    
-    print("Please enter the intervals in seconds:")
-    try:
-        interval = int(input())
-    except ValueError:
-        print("Please enter a valid number.")
-    print("Please enter the number of repetitions:")
-    try:
-        repetitions = int(input())
-    except ValueError:
-        print("Please enter a valid number.")
-    
-    text = "This timer will track " + str(repetitions) + " repetitions of " + str(interval) + " seconds. Starting in 3,2,1,now."
-    
-    filenames = [f"primer{i}.mp3" for i in range(1, 2*repetitions + 3)]
-    text_to_speech(text, filenames[i]); i+=1
-
-    to_say = [time(ss=t) for t in np.array(range(1, repetitions + 1))*interval]
-    print(to_say)
-    
-    # wait for interval seconds
-    generate_silent_mp3(interval,filenames[i]); i+=1
-
-    for say in to_say:
-        print(say)
-        text_to_speech(str(say), filenames[i]); i+=1
-        if say != to_say[-1]:
-            wait = interval - get_mp3_duration(filenames[i-1])
-            if wait < 0:
-                delete_files(filenames)
-                raise ValueError("There is not enough time to say the time. Please increase the interval or reduce the repetition.")
-            print("Wait for", wait, "seconds.")
-            generate_silent_mp3(wait, filenames[i]); i+=1
-
-    text_to_speech("Time's up!", filenames[i]); i+=1
-
-    join_mp3_files(filenames[0:i], "timer.mp3")
-
-    delete_files(filenames)
-    
+plt.show()