f818d95a0ada39b63424c4f8cad316c0a6059b1f
arielpriarone <arielpriarone@gmail.com>
2024-06-04 17:14:57 +0200

    created draft for paper

--- notebooks/dummy.py
+++ notebooks/dummy.py
@@ -1,40 +1,29 @@
-
-from colorspacious import cspace_converter
-
-import matplotlib.pyplot as plt
+from click import style
+from matplotlib.lines import lineStyles
 import numpy as np
+import matplotlib.pyplot as plt
+from pyparsing import line
 
-import matplotlib as mpl
-cmaps = {}
+phi     = np.linspace(-180, 180, 36*2+1)
+print(phi)
+theta   = phi.copy()/180*np.pi
 
-gradient = np.arange(0,20)
-gradient = np.vstack((gradient, gradient))
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+theta_ = np.linspace(-180, 180, 100)/180*np.pi
+for phi_ in phi:
+    linewidth = 0.5 if np.mod(phi_,10) < 1 else 0.7
+    phi_ = phi_/180*np.pi
+    ax.plot(np.sin(theta_), np.cos(theta_)*np.sin(phi_), color = 'k', linewidth=linewidth)
+    ax.plot(np.cos(theta_)*np.sin(phi_), np.sin(theta_), color = 'k', linewidth=linewidth)
 
-
-def plot_color_gradients(category, cmap_list):
-    # Create figure and adjust figure height to number of colormaps
-    nrows = len(cmap_list)
-    figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22
-    fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh))
-    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
-                        left=0.2, right=0.99)
-    axs[0].set_title(f'{category} colormaps', fontsize=14)
-
-    for ax, name in zip(axs, cmap_list):
-        ax.scatter(np.arange(10), np.zeros(10), c=np.arange(10), cmap=name)
-        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
-                transform=ax.transAxes)
-
-    # Turn off *all* ticks & spines, not just the ones with colormaps.
-    for ax in axs:
-        ax.set_axis_off()
-
-    # Save colormap list for later.
-    cmaps[category] = cmap_list
-
-plot_color_gradients('Qualitative',
-                    ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2',
-                    'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b',
-                    'tab20c'])
+# hide the axes and ticks
+ax.set_frame_on(False)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
 
 plt.show()
+
+
+
+
